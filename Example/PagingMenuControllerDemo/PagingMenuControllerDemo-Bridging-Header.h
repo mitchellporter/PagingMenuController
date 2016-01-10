d50574b507cd0781705f297b765fcd43ccd96ebe
Mitchell Porter <mitchellporter@gmail.com>
2016-01-09 23:28:17 -0800

    Bridging header + installed FBGlowLabel pod

---
+++ Example/PagingMenuControllerDemo/PagingMenuControllerDemo-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  PagingMenuControllerDemo-Bridging-Header.h
+//  PagingMenuControllerDemo
+//
+//  Created by Mitchell Porter on 1/9/16.
+//  Copyright © 2016 kitasuke. All rights reserved.
+//
+
+#ifndef PagingMenuControllerDemo_Bridging_Header_h
+#define PagingMenuControllerDemo_Bridging_Header_h
+#import <FBGlowLabel/FBGlowLabel.h>
+
+
+#endif /* PagingMenuControllerDemo_Bridging_Header_h */
